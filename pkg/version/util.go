037a9d4902c73e99330a3e40ac7b9eda751d370f
Duncan McGreggor <oubiwann@gmail.com>
2025-07-08 11:12:16 -0500

    Updated version package.

--- pkg/version/util.go
+++ pkg/version/util.go
@@ -1,45 +1,118 @@
 package version
 
-// XXX Move this into geomyidia project ....
+import (
+	"encoding/json"
+	"fmt"
+	"runtime/debug"
+	"strings"
 
-import "fmt"
+	"github.com/blang/semver/v4"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/geomyidia/util/pkg/errors"
+)
 
 const na string = "N/A"
 
-// ProjectVersion data
-type ProjectVersion struct {
-	Semantic   string
-	BuildDate  string
-	GitCommit  string
-	GitBranch  string
-	GitSummary string
+type Version struct {
+	Binary         string         `json:"executable"`
+	ShortName      string         `json:"short-name"`
+	LongName       string         `json:"long-name"`
+	Description    string         `json:"description"`
+	Semantic       semver.Version `json:"version"`
+	Go             string         `json:"go-version"`
+	BuildPath      string         `json:"build-path"`
+	CurrentBranch  string         `json:"current-branch"`
+	CommitID       string         `json:"commid-id"`
+	BuildTime      string         `json:"build-time"`
+	GitSummary     string         `json:"git-summery"`
+	DeployedFromIP string         `json:"deployed-from-ip"`
+}
+
+func New(
+	version,
+	shortName,
+	longName,
+	desc,
+	branch,
+	commitID,
+	buildTime,
+	gitSummary,
+	deployedFromIP string,
+) (*Version, error) {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return nil, errors.Combine(errors.ErrVersionData, errors.ErrBuildInfo)
+	}
+	sem, err := semver.Parse(version)
+	if err != nil {
+		return nil, errors.Combine(errors.ErrVersionData, err)
+	}
+
+	short := strings.TrimSpace(shortName)
+	v := &Version{
+		Binary:        short,
+		ShortName:     short,
+		LongName:      strings.TrimSpace(longName),
+		Description:   strings.TrimSpace(desc),
+		Semantic:      sem,
+		BuildPath:     bi.Path,
+		CurrentBranch: strings.TrimSpace(branch),
+		CommitID:      strings.TrimSpace(commitID),
+		BuildTime:     strings.TrimSpace(buildTime),
+		GitSummary:    strings.TrimSpace(gitSummary),
+		Go:            bi.GoVersion,
+	}
+	if deployedFromIP != "" {
+		v.DeployedFromIP = strings.TrimSpace(deployedFromIP)
+	} else {
+		v.DeployedFromIP = na
+	}
+	return v, nil
 }
 
 // BuildString ...
-func BuildString(version *ProjectVersion) string {
-	if version.GitCommit == "" {
+func (version *Version) BuildString() string {
+	if version.CommitID == "" {
 		return na
 	}
-	return fmt.Sprintf("%s@%s, %s", version.GitBranch, version.GitCommit, version.BuildDate)
+	return fmt.Sprintf("%s@%s, %s", version.CurrentBranch, version.CommitID, version.BuildTime)
 }
 
 // String ...
-func String(version *ProjectVersion) string {
-	if version.Semantic == "" {
-		return na
+func (version *Version) String() string {
+	return version.Semantic.String()
+}
+
+func (version *Version) AsJSON() string {
+	data, err := json.Marshal(version)
+	if err != nil {
+		log.Error(errors.Combine(errors.ErrVersionData, err))
 	}
-	return version.Semantic
+	return string(data)
+}
+
+func (version *Version) Major() int {
+	return int(version.Semantic.Major)
+}
+
+func (version *Version) Minor() int {
+	return int(version.Semantic.Minor)
+}
+
+func (version *Version) Patch() int {
+	return int(version.Semantic.Patch)
+}
+
+func (version *Version) Micro() int {
+	return version.Patch()
 }
 
 // VersionedBuildString ...
-func VersionedBuildString(version *ProjectVersion) string {
-	v := version.Semantic
-	gc := version.GitCommit
-	if v == "" {
-		v = na
-	}
+func (version *Version) VersionedBuildString() string {
+	gc := version.CommitID
 	if gc == "" {
 		gc = na
 	}
-	return fmt.Sprintf("%s, %s@%s, %s", v, version.GitBranch, gc, version.BuildDate)
+	return fmt.Sprintf("%s, %s@%s, %s", version.String(), version.CurrentBranch, gc, version.BuildTime)
 }
